f2f0172a86a5164d96846affc0ce0eee70a91204
ayush ranjan <asayushranjansinha@gmail.com>
2024-05-23 20:10:37 +0530

    footer layout changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,46 +4,54 @@ import { Separator } from "./ui/separator";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto py-6 bg-[#FEF8EC]">
-      <div className="w-full grid grid-cols-2 md:grid-cols-4">
-        <div className="">
-          <Link className="flex items-center" to={"/"}>
-            <span className="font-semibold text-2xl tracking-tighter">
-              <span className="text-pink-500">Dil Se</span>
-              <span className="text-yellow-500"> Daawat</span>
-            </span>
+    <footer className="container mx-auto bg-[#fff4f4] py-6">
+      <div className="grid md:grid-cols-3 gap-y-3 md:justify-items-center">
+        <div>
+          <Link to="/" className="flex items-center">
             <img
-              src="./bowl.svg"
-              alt="Bowl Icon"
-              className="h-8 w-8 ml-1"
+              src="https://res.cloudinary.com/dn6ze90sb/image/upload/v1716472053/logo_vtpnug.svg"
+              alt="Dil Se Daawat Logo"
+              className="h-8 w-8 shrink-0"
             />
+            <span className="text-2xl font-semibold tracking-tight ml-2 text-brand-primary whitespace-nowrap">
+              dil se <span className="text-pink-500">daawat</span>
+            </span>
           </Link>
-          <address className="font-medium text-gray-700">
-            Lorem ipsum dolor <br />
-            sit amet random.
-          </address>
-
+          <p className="font-medium text-gray-700 capitalize whitespace-nowrap">
+            Delivering Smiles since 2023
+          </p>
+        </div>
+        <div className="">
+          <h5 className="text-neutral-900 font-bold">Get In Touch</h5>
+          <span className="mt-4 text-gray-700 font-medium">
+            (+91) 8709410000
+          </span>
           <div className="flex flex-row gap-3 py-3">
             <div className="bg-white rounded-full p-2 drop-shadow-md cursor-pointer">
-                <InstagramIcon />
+              <InstagramIcon />
             </div>
             <div className="bg-white rounded-full p-2 drop-shadow-md cursor-pointer">
-                <Twitter />
+              <Twitter />
             </div>
             <div className="bg-white rounded-full p-2 drop-shadow-md cursor-pointer">
-                <Facebook />
+              <Facebook />
             </div>
           </div>
         </div>
-        <div className="">
-            <h5 className="text-neutral-900 font-bold">Get In Touch</h5>
-            <span className="mt-4 text-gray-700 font-medium">(+91)8709410000</span>
+
+        <div className="flex flex-col text-sm">
+          <span className="hover:underline font-medium trnsition cursor-pointer">
+            Privacy Policy
+          </span>
+          <span className="hover:underline font-medium trnsition cursor-pointer">
+            Terms & Conditions
+          </span>
         </div>
-        <span className="text-neutral-900 font-bold">Privacy Policy</span>
-        <span className="text-neutral-900 font-bold">Terms & Cond.</span>
       </div>
-      <Separator className="my-2"/>
-      <p className="w-full text-center font-medium mt-2">&copy; 2024 Dil Se Daawat. All Rights Reserved</p>
+      <Separator className="my-2" />
+      <p className="w-full text-center font-medium mt-2">
+        &copy; 2024 Dil Se Daawat. All Rights Reserved
+      </p>
     </footer>
   );
 };
