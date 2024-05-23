f40af6d126f9be40bb0ce005d79204cbe7d63435
ayush ranjan <asayushranjansinha@gmail.com>
2024-05-23 20:11:14 +0530

    header layout changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,25 +9,24 @@ const Header = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    <header className="sticky inset-x-0 top-0 z-50 opacity-90 backdrop-blur-md py-3 bg-gradient-to-b from-[#fef8ea] to-[#fef8eac4]">
+    <header className="sticky inset-x-0 top-0 z-50 opacity-90 backdrop-blur-md py-3 bg-[#fff4f4]">
       <div className="container mx-auto flex justify-between items-center gap-6">
-        <Link className="flex items-center" to={"/"}>
-          <span className="font-semibold text-2xl tracking-tighter">
-            <span className="text-pink-500">Dil Se</span>
-            <span className="text-yellow-500"> Daawat</span>
-          </span>
+        <Link to="/" className="flex items-center">
           <img
-            src="./bowl.svg"
-            alt="Bowl Icon"
-            className="h-8 w-8 ml-1"
+            src="https://res.cloudinary.com/dn6ze90sb/image/upload/v1716472053/logo_vtpnug.svg"
+            alt="Dil Se Daawat Logo"
+            className="h-8 w-8 shrink-0"
           />
+          <span className="text-2xl font-semibold tracking-tight ml-2 text-brand-primary whitespace-nowrap">
+            dil se <span className="text-pink-500">daawat</span>
+          </span>
         </Link>
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <MobileNav />
         </div>
 
         {/* Main Navigation / Desktop Navigation */}
-        <nav className="hidden md:flex items-center justify-between gap-4">
+        <nav className="hidden lg:flex items-center justify-between gap-4">
           <Link className="main-navlink" to="#hero">
             Home
           </Link>
@@ -44,7 +43,8 @@ const Header = () => {
             Contact Us
           </Link>
         </nav>
-        <div className="hidden md:flex items-center gap-3">
+
+        <div className="hidden lg:flex items-center gap-3">
           <Button variant={"ghost"} size={"icon"}>
             <Search />
           </Button>
